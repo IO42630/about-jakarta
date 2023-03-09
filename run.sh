029235e77906d4cfc1473a296840e3d82d71b80c
io42630 <ivan@olexyn.com>
2023-03-09 09:19:45 +0100

    ~ expand run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-docker build -t about-jakarta -f docker/Dockerfile . &&
-docker compose up
+# This script is mean to build and redeploy all WARs.
+
+# STOP all docker
+docker stop $(docker ps -a -q) &&
+# build all
+cd jee-web-app &&
+mvn clean install -DskipTests=true &&
+cd .. &&
+# build docker image (this will also copy the WARs)
+docker build -t about-jakarta:latest -f docker/Dockerfile . &&
+# START all docker
+docker compose up --force-recreate
