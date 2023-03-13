0c91b4eeab2b5dcdc8f2056c458cd102439c922f
io42630 <ivan@olexyn.com>
2023-03-13 23:08:01 +0100

    + fix run

--- run.sh
+++ run.sh
@@ -2,12 +2,20 @@
 
 # This script is mean to build and redeploy all WARs.
 
+WS=${PWD}
+
 # STOP all docker
 docker stop $(docker ps -a -q) &&
 # build all
 cd jee-web-app &&
 mvn clean install -DskipTests=true &&
-cd .. &&
+cd ${WS} &&
+cd security/security-ctx &&
+mvn clean install -DskipTests=true &&
+cd ${WS} &&
+cd security/ws-basic &&
+mvn clean install -DskipTests=true &&
+cd ${WS} &&
 # build docker image (this will also copy the WARs)
 docker build -t about-jakarta:latest -f docker/Dockerfile . &&
 # START all docker
